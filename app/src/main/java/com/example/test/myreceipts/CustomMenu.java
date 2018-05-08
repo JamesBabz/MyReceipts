88bd331fad26be623c33a70cc4d2f408a0205cc1
Enemark96 <je.enemark96@gmail.com>
2018-05-08 11:04:03 +0200

    MenuItem Create Category
    MenuItem Create Category

--- app/src/main/java/com/example/test/myreceipts/CustomMenu.java
+++ app/src/main/java/com/example/test/myreceipts/CustomMenu.java
@@ -1,14 +1,23 @@
 package com.example.test.myreceipts;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.example.test.myreceipts.BLL.UserService;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Jacob Enemark on 07-05-2018.
@@ -17,6 +26,11 @@ import com.google.firebase.auth.FirebaseAuth;
 public class CustomMenu extends AppCompatActivity {
 
     UserService mUserService;
+    private String m_Text = "";
+
+
+    private FirebaseAuth fAuth;
+    private FirebaseFirestore db;
     boolean backBtn;
     boolean profileMenuItem;
 
@@ -25,6 +39,9 @@ public class CustomMenu extends AppCompatActivity {
         mUserService = new UserService();
         this.backBtn = backBtn;
         this.profileMenuItem = profileMenuItem;
+
+        fAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
@@ -57,6 +74,9 @@ public class CustomMenu extends AppCompatActivity {
                 case R.id.optProfile:
                 openProfileView();
                 return true;
+            case R.id.optCreateCategory:
+                createCategory();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -72,5 +92,49 @@ public class CustomMenu extends AppCompatActivity {
         Intent intent = new Intent(CustomMenu.this, ProfileActivity.class);
         intent.putExtra("USER", mUserService.getCurrentUser().getUid());
         startActivity(intent);
+    }
+
+    private void createCategory()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Create category");
+
+    // Set up the input
+        final EditText input = new EditText(this);
+    // Specify which input type
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+    // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                //Text input for AlertDialog
+                m_Text = input.getText().toString();
+
+                //creates exist field
+                Map<String, Boolean> exists = new HashMap<>();
+                exists.put("exists", true);
+                db.collection("users").document(getCurrentUser().getUid()).collection("categories").document(m_Text).set(exists);
+
+                //Refresh page for new categories
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    public FirebaseUser getCurrentUser(){
+        return fAuth.getCurrentUser();
     }
 }
