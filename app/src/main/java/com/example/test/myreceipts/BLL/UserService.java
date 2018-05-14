d0769a1c50e022c4b2688c4156c3def3f5037e9d
JamesBabz <web.fuhlendorff@gmail.com>
2018-05-14 10:10:01 +0200

    prettify
    pretty code

--- app/src/main/java/com/example/test/myreceipts/BLL/UserService.java
+++ app/src/main/java/com/example/test/myreceipts/BLL/UserService.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -94,10 +95,12 @@ public class UserService {
     public void setStandardFolders(){
         Map<String, Boolean> exists = new HashMap<>();
         exists.put("exists", true);
-        db.collection("users").document(getCurrentUser().getUid()).collection("categories").document("favorites").set(exists);;
-        db.collection("users").document(getCurrentUser().getUid()).collection("categories").document("favorites").collection("fileuids").document("0").set(exists);;
-        db.collection("users").document(getCurrentUser().getUid()).collection("categories").document("unassigned").set(exists);;
-        db.collection("users").document(getCurrentUser().getUid()).collection("categories").document("unassigned").collection("fileuids").document("0").set(exists);;
+        CollectionReference catReference = db.collection("users").document(getCurrentUser().getUid()).collection("categories");
+
+        catReference.document("favorites").set(exists);
+        catReference.document("favorites").collection("fileuids").document("0").set(exists);
+        catReference.document("unassigned").set(exists);
+        catReference.document("unassigned").collection("fileuids").document("0").set(exists);
     }
 
 }
