954afc4e5930dec456163ce7581460728f2e1ffa
thomasmeyerh <thom953b@easv365.dk>
2018-05-22 12:57:12 +0200

    Open full image from recent

--- app/src/main/java/com/example/test/myreceipts/MainActivity.java
+++ app/src/main/java/com/example/test/myreceipts/MainActivity.java
@@ -28,7 +28,9 @@ import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 import butterknife.BindView;
@@ -83,7 +85,6 @@ public class MainActivity extends CustomMenu {
 
         mStore = FirebaseFirestore.getInstance();
         mStorage = FirebaseStorage.getInstance().getReference();
-
 
     }
 
@@ -248,7 +249,24 @@ public class MainActivity extends CustomMenu {
             @Override
             public void run() {
                 // the 4 image views are a added to a [], each time it gets through the loop, it will get the next imageview to set a new image.
+              //  images[x].setImageBitmap(imageHandler.getImageBitmap(uri.toString()));
                 images[x].setImageBitmap(imageHandler.getImageBitmap(uri.toString()));
+                HashMap<ImageView, String> map = new HashMap<>();
+                map.put(images[x], uri.toString());
+
+                for(final Map.Entry<ImageView, String> entry : map.entrySet()) {
+                   entry.getKey().setOnClickListener(new View.OnClickListener() {
+                       @Override
+                       public void onClick(View view) {
+                           Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+                           Uri data = Uri.parse(entry.getValue());
+                           intent.setDataAndType(data, "image/*");
+                           startActivity(intent);
+
+                       }
+                   });
+
+                }
                 x++;
             }
         });
